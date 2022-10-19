b0e630b6ceac25dd690c2971751be7ef4cfcefad
Zioben1976 <zioben@inwind.it>
2022-10-19 20:30:28 +0200

    Bitplane

--- ColourClashNet/Color/ColorIntExt.cs
+++ ColourClashNet/Color/ColorIntExt.cs
@@ -12,7 +12,9 @@ namespace ColourClashNet.Colors
         public static System.Drawing.Color ToDrawingColor(this int i)
         {
             if (i < 0)
+            {
                 return System.Drawing.Color.Transparent;
+            }
             unchecked
             {
                 return System.Drawing.Color.FromArgb(i | (int)0xFF_00_00_00);
@@ -32,16 +34,66 @@ namespace ColourClashNet.Colors
             return i >= 0 ? (i >> 0) & 0x00ff : -1;
         }
 
+        public static float ToH(this int i)
+        {
+            if (i < 0)
+                return float.MaxValue;
+            var Col = ToDrawingColor(i);
+            return Col.GetHue();
+        }
+
+        public static float ToS(this int i)
+        {
+            if (i < 0)
+                return float.MaxValue;
+            var Col = ToDrawingColor(i);
+            return Col.GetSaturation() * 100.0f;
+        }
+
+        public static float ToV(this int i)
+        {
+            if (i < 0)
+                return float.MaxValue;
+            var Col = ToDrawingColor(i);
+            return Col.GetBrightness() * 100.0f;
+        }
+
 
         public static double Distance(this int i,  int j, ColorDistanceEvaluationMode eMode)
         {
             if (i < 0 || j < 0)
                 return double.PositiveInfinity;
-            int R = i.ToR() - j.ToR();
-            int G = i.ToG() - j.ToG();
-            int B = i.ToB() - j.ToB();
-            return R * R + G * G + B * B;
+            switch (eMode)
+            {
+                case ColorDistanceEvaluationMode.RGB:
+                    {
+                        int R = i.ToR() - j.ToR();
+                        int G = i.ToG() - j.ToG();
+                        int B = i.ToB() - j.ToB();
+                        return R * R + G * G + B * B;
+                    }
+                case ColorDistanceEvaluationMode.HSV:
+                    {
+                        var H = i.ToH() - j.ToH();
+                        var S = i.ToS() - j.ToS();
+                        var V = i.ToV() - j.ToV();
+                        return H * H + S * S + V * V;
+                    }
+                case ColorDistanceEvaluationMode.All:
+                    {
+                        int R = i.ToR() - j.ToR();
+                        int G = i.ToG() - j.ToG();
+                        int B = i.ToB() - j.ToB();
+                        var H = i.ToH() - j.ToH();
+                        var S = i.ToS() - j.ToS();
+                        var V = i.ToV() - j.ToV();
+                        return R * R + G * G + B * B + H * H + S * S + V * V;
+                    }
+                default:
+                    return double.PositiveInfinity;
+            }
         }
+
         public static int FromRGB( int r, int g , int b)
         {
             if (r<0 || g <0 || b <0 )
