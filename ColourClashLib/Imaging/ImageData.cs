37943444cdb3207a5197645807535ea2ad62cf49
zioben <ziobe@DANASCULLY>
2026-01-13 11:53:43 +0100

    minor changes

--- ColourClashLib/Imaging/ImageData.cs
+++ ColourClashLib/Imaging/ImageData.cs
@@ -2,6 +2,7 @@
 using ColourClashNet.Defaults;
 using System;
 using System.Collections.Generic;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +10,13 @@ using System.Threading.Tasks;
 namespace ColourClashNet.Imaging
 {
     [Serializable]
+       
     public partial class ImageData
     {
         public string Name { get; set; } = "";
         public int[,]? Data { get; set; }
-        public Palette Palette { get; set; } = new Palette();
-        public Histogram Histogram { get; set; } = new Histogram();
+        public Palette ColorPalette { get; set; } = new Palette();
+        public Histogram ColorHistogram { get; set; } = new Histogram();
 
         public int Width
         {
@@ -40,15 +42,66 @@ namespace ColourClashNet.Imaging
             {
                 return Width * Height;
             }
-        }   
+        }
 
-        public int ColorCount
+        public int Colors
         {
             get
             {
-                return Palette.Count;
+                return ColorPalette.Count;
             }
         }
 
+        
+        public bool Reset()
+        {
+            Data = null;
+            ColorPalette = new Palette();
+            ColorHistogram = new Histogram();
+            return true;
+        }
+
+
+        public async Task<bool> SetDataAsync(int[,] oData, CancellationToken? oToken)
+        {
+            string sM = nameof(SetDataAsync);
+            if (oData == null)
+            {
+                return false;
+            }
+            Data = oData.Clone() as int[,];
+            ColorHistogram = await Histogram.CreateColorHistogramAsync(oData, oToken);
+            ColorPalette = ColorHistogram.ToColorPalette();
+            return true;
+        }
+
+        public async Task<bool> SetDataAsync(System.Drawing.Image oImage, CancellationToken? oToken)
+        {
+            string sM = nameof(SetDataAsync);
+            if (oImage == null)
+            {
+                return false;
+            }
+            var oImageRaw = ImageTools.BitmapToMatrix(oImage as System.Drawing.Bitmap);
+            return await SetDataAsync(oImageRaw, oToken);
+        }
+
+        public async Task<bool> SetDataAsync(string sFileName, CancellationToken? oToken)
+        {
+            string sM = nameof(SetDataAsync);
+            if (!File.Exists(sFileName))
+            {
+                return false;
+            }
+            var oBitmap = ImageTools.BitmapFromFile(sFileName);
+            return await SetDataAsync(oBitmap, oToken);
+        }
+
+        public bool Save(string sFileName, ImageExportFormat eFormat)
+        {
+            string sM = nameof(Save);
+            var oBmp = ImageTools.MatrixToBitmap(Data);
+            return ImageTools.BitmapToFile(oBmp, sFileName, eFormat);
+        }
     }
 }
